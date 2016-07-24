e8df87889c618b9626d560ebfdc9238f5f39d00f
Parrikar <parrikaa@pun-parrikaa-m.ds.ad.adp.com>
2016-07-24 13:07:54 +0530

    gulp-setup

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+/**
+ * Created by parrikaa on 24/07/16.
+ */
+'use strict';
+
+var gulp = require('gulp'),
+    wrench = require('wrench');
+
+wrench.readdirSyncRecursive('./gulp-tasks')
+    .filter(function (file) {
+        return (/\.(js)$/i).test(file);
+    })
+    .map(function (file) {
+        require('./gulp-tasks/' + file);
+    });
+
+gulp.task('default', ['serve']);
